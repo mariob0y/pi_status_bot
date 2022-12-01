ac4cbbc1e7c576c0ba17a168fc66252cea3526d6
Andrew <suroviymarioboy@gmail.com>
2022-12-01 10:55:14 +0200

    initial commit

---
+++ bot.py
@@ -0,0 +1,53 @@
+from pexpect import pxssh
+import telebot
+from datetime import datetime
+import time
+from apscheduler.schedulers.background import BackgroundScheduler
+from const import TOKEN, SSH_IP, SSH_USERNAME, SSH_PASSWORD
+
+telebot.apihelper.ENABLE_MIDDLEWARE = True
+bot = telebot.TeleBot(TOKEN)
+scheduler = BackgroundScheduler()
+
+conf = {"first_message": True, "last_check": "OFFLINE"}
+
+
+def pi_is_online():
+    try:
+        ssh = pxssh.pxssh()
+        login = ssh.login(SSH_IP, SSH_USERNAME, SSH_PASSWORD)
+        time.sleep(5)
+        if login:
+            ssh.logout()
+            return True
+        else:
+            return False
+    except:
+        return False
+
+
+def send_update(message):
+    status = "ONLINE" if pi_is_online() else "OFFLINE"
+    if conf.get("first_message"):
+        text = f"Pi is {status} as of {str(datetime.now())}"
+        conf["first_message"] = False
+        conf["last_check"] = status
+        bot.send_message(message.from_user.id, text)
+    else:
+        if conf.get("last_check") != status:
+            text = f"Pi went {status} at {str(datetime.now())}"
+            conf["last_check"] = status
+            bot.send_message(message.from_user.id, text)
+
+
+@bot.message_handler(commands=["start"])
+def start_message(message):
+    scheduler.add_job(send_update, args=(message,), trigger="interval", minutes=3)
+    bot.send_message(message.from_user.id, "Welcome to Pi check")
+
+
+if __name__ == "__main__":
+    # for local run
+    scheduler.start()
+    bot.remove_webhook()
+    bot.infinity_polling(none_stop=True, interval=1)
---
+++ const.py
@@ -0,0 +1,6 @@
+import os
+
+TOKEN = os.environ.get("TELEGRAM_TOKEN", "")
+SSH_IP = os.environ.get("SSH_IP", "")
+SSH_USERNAME = os.environ.get("SSH_USERNAME", "")
+SSH_PASSWORD = os.environ.get("SSH_PASSWORD", "")
